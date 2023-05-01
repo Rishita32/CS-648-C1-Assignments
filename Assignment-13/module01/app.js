4e4aaf03c966c5cf7367a9296a4d24e45caa2ace
Rishita32 <rishitamote@gmail.com>
2023-04-30 21:33:05 -0700

    assignment 13 module 01

--- Assignment-13/module01/app.js
+++ Assignment-13/module01/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  console.log("gere");
+  console.log("here");
   res.sendFile(__dirname + "/public/index.html");
 });
 
